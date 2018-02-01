5e55306426f5fb9f45c687ae6cd91fec9bcbf17e
Seungmoo <dltmdan92@gmail.com>
2018-02-01 13:49:04 +0000

    second commit by lsm

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var static = require('serve-static');
+var errorHandler = require('errorhandler');
+
+var expressErrorHandler = require('express-error-handler');
+var expressSession = require('express-session');
+
+var mysql = require('mysql');
